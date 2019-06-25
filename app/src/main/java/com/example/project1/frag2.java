48b01a2231c8379aa78dc07987e7032a752e33bd
yudahendriawan <yudahendriawan007@gmail.com>
2019-06-25 09:41:28 +0700

    frag2

--- app/src/main/java/com/example/project1/frag2.java
+++ app/src/main/java/com/example/project1/frag2.java
@@ -14,38 +14,67 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
+import static com.example.project1.R.layout.frag2_layout;
+
 public class frag2 extends Fragment {
-    private RecyclerView recyclerView;
-    private DataAdapter adapter;
-    private ArrayList<DataParagrafInput> DataArrayList;
+
     @Nullable
     @Override
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.frag2_layout,container,false );
+        return inflater.inflate(frag2_layout,container,false );
 
 
     }
+
+    private RecyclerView rvView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private ArrayList<String> dataSet;
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        recyclerView = (RecyclerView) view.findViewById(R.id.recyler_view);
 
-        adapter = new DataAdapter(DataArrayList);
+        dataSet = new ArrayList<>();
+        initDataset();
 
+        rvView = (RecyclerView) view.findViewById(R.id.rv_main);
+        rvView.setHasFixedSize(true);
 
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
-        recyclerView.setLayoutManager(layoutManager);
-        recyclerView.setAdapter(adapter);
+        /**
+         * Kita menggunakan LinearLayoutManager untuk list standar
+         * yang hanya berisi daftar item
+         * disusun dari atas ke bawah
+         */
+        layoutManager = new LinearLayoutManager(this.getContext());
+        rvView.setLayoutManager(layoutManager);
+
+        adapter = new RecyclerViewAdapter(dataSet);
+        rvView.setAdapter(adapter);
 
     }
-    void addData(){
-        DataArrayList = new ArrayList<>();
-        DataArrayList .add(new DataParagrafInput("FIRMAN"));
-        DataArrayList .add(new DataParagrafInput("syah"));
-        DataArrayList.add(new DataParagrafInput("ilham"));
-        DataArrayList .add(new DataParagrafInput("dwi"));
+
+    private void initDataset(){
+
+        /**
+         * Tambahkan item ke dataset
+         * dalam prakteknya bisa bermacam2
+         * tidak hanya String seperti di kasus ini
+         */
+        dataSet.add("Karin");
+        dataSet.add("Ingrid");
+        dataSet.add("Helga");
+        dataSet.add("Renate");
+        dataSet.add("Elke");
+        dataSet.add("Ursula");
+        dataSet.add("Erika");
+        dataSet.add("Christa");
+        dataSet.add("Gisela");
+        dataSet.add("Monika");
 
     }
 }
+
+
+
